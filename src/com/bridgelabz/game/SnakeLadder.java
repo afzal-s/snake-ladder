7b2101137159fbf44ec403965852944429cdec92
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-15 12:44:11 +0530

    Solved: UC2 ~ Generate Random Dice Number.

--- src/com/bridgelabz/game/SnakeLadder.java
+++ src/com/bridgelabz/game/SnakeLadder.java
@@ -1,12 +1,28 @@
 package com.bridgelabz.game;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class SnakeLadder {
 
 	public static void main(String[] args) {
 		int startPosition = 0, numberOfPlayer = 1;
-		
+
 		System.out.println("Number Of Player: " + numberOfPlayer);
 		System.out.println("Current Position: " + startPosition);
-	}
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Press 1 To Roll Dice: ");
+		int number = scanner.nextInt();
+
+		if (number == 1) {
+			Random random = new Random();
+			int dice = 1 + random.nextInt(6);
+			System.out.println("Dice Rolling...");
+			System.out.println("Dice Number: " + dice);
+		} else {
+			System.out.println("Error: Run The Program Again & Enter Valid Number");
+		}
+
+	}
 }
