4386a4b24e6c082b9d8b3d32fb3edf7b43c94eae
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-09-15 12:26:05 +0530

    Solved: UC1 ~ Single Player At Position Zero.

--- src/com/bridgelabz/game/SnakeLadder.java
+++ src/com/bridgelabz/game/SnakeLadder.java
@@ -3,7 +3,10 @@ package com.bridgelabz.game;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Snake & Ladder Simulator.");
+		int startPosition = 0, numberOfPlayer = 1;
+		
+		System.out.println("Number Of Player: " + numberOfPlayer);
+		System.out.println("Current Position: " + startPosition);
 	}
 
 }
